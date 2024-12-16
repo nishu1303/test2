f638dd889e392b9f3d37ea98e7aded90967cf53f
Nishma Vasireddy <nishma.vasireddy@gmail.com>
2024-12-16 09:55:16 +0530

    track all files

---
+++ sample idle 3 24-10-2024.py
@@ -0,0 +1,24 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: D:/python pratice/test 24-4-2024.py =================
+hi
+30 is the greatest
+15
+25
+False
+False
+False
+True
+True
+True
+False
+False
+True
+False
+True
+True
+False
+False
+True
+True
