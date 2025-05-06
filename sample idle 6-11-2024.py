cb7ae6d595cf330cf6faa4f549702fb6aebe35da
nishu1303 <96492876+nishu1303@users.noreply.github.com>
2025-05-06 12:38:11 +0530

    push in sample file

---
+++ sample idle 6-11-2024.py
@@ -0,0 +1,225 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 1, in <module>
+    conditionalStatment
+NameError: name 'conditionalStatment' is not defined
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 1, in <module>
+    if score>80:
+NameError: name 'score' is not defined
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 7, in <module>
+    elif score (60-80):
+TypeError: 'int' object is not callable
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 7, in <module>
+    elif score (60):
+TypeError: 'int' object is not callable
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 7, in <module>
+    elif score (60-80):
+TypeError: 'int' object is not callable
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 7, in <module>
+    elif score (65):
+TypeError: 'int' object is not callable
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 7, in <module>
+    if score (60-80):
+TypeError: 'int' object is not callable
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 7, in <module>
+    elif score (35-60):
+TypeError: 'int' object is not callable
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+30
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+30
+-20
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+30
+-20
+600
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+30
+-20
+600
+True
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+30
+-20
+600
+True
+False
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+30
+-20
+600
+True
+False
+True
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 33, in <module>
+    f=fail
+NameError: name 'fail' is not defined
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Grade B
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+class 1st
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 4, in <module>
+    Print ('Grade A')
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 4, in <module>
+    Print ("Grade A")
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+Grade A
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 57, in <module>
+    if a!=b:
+NameError: name 'a' is not defined
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+True
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+True
+condition failed
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 70, in <module>
+    print ("xyz",false)
+NameError: name 'false' is not defined. Did you mean: 'False'?
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+True
+condition failed
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+True
+condition failed
+hello else
+out of else
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+True
+condition failed
+hello else
+out of else
+xyz
+Traceback (most recent call last):
+  File "D:/python pratice/test 6-11-2024.py", line 75, in <module>
+    print ("xyz",true)
+NameError: name 'true' is not defined. Did you mean: 'True'?
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+True
+condition failed
+hello else
+out of else
+xyz
+xyz True
+out of else
+
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+True
+condition failed
+hello else
+out of else
+xyz
+xyz True
+out of else
+>>> 
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+True
+condition failed
+hello else
+out of else
+xyz
+xyz 30
+out of else
+>>> 
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+True
+condition failed
+hello else
+out of else
+xyz
+xyz 30
+out of else
+>>> 
+================= RESTART: D:/python pratice/test 6-11-2024.py =================
+conditional
+xyz
+True
+condition failed
+hello else
+out of else
+xyz
+xyz 30
+out of else
+xyz
+xyz 30
+out of else
